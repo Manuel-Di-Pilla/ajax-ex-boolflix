062a2d452d996f3b7dd056c603584c600078283c
Manuel <manueldipilla3@gmail.com>
2020-02-06 11:29:06 +0100

    milestone 1 completata

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $(document).on('click', '.button', function () {
+    var ricerca = $('.search').val();
+    $.ajax(
+      {
+        url: 'https://api.themoviedb.org/3/search/movie',
+        method: 'GET',
+        data: {
+          api_key: '37d5c5ef82f75ce59c3d75b9a0da47e4',
+          language: 'it-IT',
+          query: ricerca,
+        },
+        success: function (data) {
+          var film = data.results;
+          dataFilm(film);
+        },
+        error: function () {
+          alert('errore');
+        }
+      }
+    );
+  });
+});
+
+function dataFilm (data) {
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < data.length; i++) {
+    var thisData = data[i];
+    var html = template(thisData);
+    $('.film').append(html);
+  }
+}
