9be08660842f7dae8a21329fdc363b356aa265d1
Manuel <manueldipilla3@gmail.com>
2020-02-07 15:59:24 +0100

    Aggiunta stelle

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   $(document).on('click', '.button', function () {
-    $('.film').html('');
+    resetSearch();
     searchFilm();
   });
   $('.search').keyup(function () {
-    if (event.keyCode == 13) {
-      $('.film').html('');
+    if (event.which == 13 || event.keyCode == 13) {
+      resetSearch();
       searchFilm();
     }
   });
@@ -38,12 +38,55 @@ function dataFilm (data) {
   var template = Handlebars.compile(source);
   for (var i = 0; i < data.length; i++) {
     var thisData = data[i];
-    var html = template(thisData);
+    var vote = Math.ceil(thisData.vote_average / 2);
+    var context = {
+      title: thisData.title,
+      original_title: thisData.original_title,
+      original_language: thisData.original_language,
+      vote_average: vote,
+    }
+    var html = template(context);
     $('.film').append(html);
   }
   if (data.length <= 0) {
     var context = {notfound:'Nessun risultato trovato'};
     var html = template(context);
     $('.film').append(html);
+  }
+  star(data);
+}
+function resetSearch() {
+  $('.film').html('');
+}
+function star(data) {
+  for (var i = 0; i < data.length; i++) {
+    var thisList = $('ul').eq(i).find('li#voto').text();
+    var thisStar = $('ul').eq(i).find('li#voto span i');
+
+    if (thisList == 1) {
+      thisStar.eq(0).css('color', 'red').removeClass('far').addClass('fas');
+    }
+    else if (thisList == 2) {
+      thisStar.eq(0).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(1).css('color', 'red').removeClass('far').addClass('fas');
+    }
+    else if (thisList == 3) {
+      thisStar.eq(0).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(1).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(2).css('color', 'red').removeClass('far').addClass('fas');
+    }
+    else if (thisList == 4) {
+      thisStar.eq(0).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(1).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(2).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(3).css('color', 'red').removeClass('far').addClass('fas');
+    }
+    else if (thisList == 5) {
+      thisStar.eq(0).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(1).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(2).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(3).css('color', 'red').removeClass('far').addClass('fas');
+      thisStar.eq(4).css('color', 'red').removeClass('far').addClass('fas');
+    }
   }
 }
